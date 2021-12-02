19c255ff99142dbcaf08c4e62e28bc726fadb574
Matthew Sheehan <cementfactorypublishing@gmail.com>
2021-12-01 19:14:38 -0500

    created game manager
    Game Manager is a singleton which holds data for overall game: including scenes and scene changes, game states. players cards.
    Also added a button to top left of screen to debug scene changing

---
+++ Assets/__Scripts/RATC scripts/Singleton.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Generic singleton type class to be derived 
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Singleton<T> : MonoBehaviour where T : Component
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject();
+                    obj.name = typeof(T).Name;
+                    _instance = obj.AddComponent<T>();
+                }
+            }
+            return _instance;
+
+        }
+    }
+    /// <summary>
+    /// Make sure only one instance exists. 
+    /// Existing instance wont get destroyed on scene loading
+    /// </summary>
+    public virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
